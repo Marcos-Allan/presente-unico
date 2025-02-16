3f77b7f631c545fb7614536315032bb5bbf31cab
Marcos Allan <allanmenezes880@gmail.com>
2025-02-16 17:26:29 -0300

    correção de erro

--- src/components/NetworkConnection/index.tsx
+++ src/components/NetworkConnection/index.tsx
@@ -3,9 +3,11 @@ import { useNetworkStatus } from "use-network-status";
 
 export default function NetworkStatus({ children }: { children: React.ReactNode }) {
     const { online }: any = useNetworkStatus();
-    const isLocalhost = window.location.hostname.includes("http://localhost:5173")
+    const isLocalhost = window.location.hostname.includes("localhost")
+    
+    console.log(isLocalhost)
 
-  if (!online && isLocalhost == true) {
+  if (!online && isLocalhost == false) {
     return (
         <div className={`w-screen h-screen flex items-center justify-center text-my-secondary text-[48px] font-bold uppercase`}>
             Offline
